b14b6e9cc15dcdca78781b173568eb819854beac
MichelleRobson <michellelizrobson@gmail.com>
2020-10-29 00:21:45 +0530

    main file

--- sketch.js
+++ sketch.js
@@ -54,14 +54,11 @@ function setup() {
 function draw() {
 
   background(230);
-  //frameRate(2)
- // Engine.update(engine)
+  
   textSize(25);
   text("Press Space to get a second Chance to Play!!",50 ,50);
   image(boy ,200,340,200,300);
-  //Engine.update(engine)
   
-
   treeObj.display();
   stoneObj.display();
   mango1.display();
@@ -69,7 +66,7 @@ function draw() {
   mango3.display();
   mango4.display();
   mango6.display();
- mango7.display();
+  mango7.display();
   mango8.display();
   mango9.display();
   mango10.display();
@@ -112,21 +109,13 @@ function keyPressed() {
   }
 
   function detectollision(lstone,lmango){
-	/*var collision = Matter.SAT.collides(lstone,lmango);
-	if(collision.collided){
-		console.log("collided");
-		Matter.Body.setStatic(lmango,false);	
-	}*/
-  mangoBodyPosition=lmango.body.position
+	mangoBodyPosition=lmango.body.position
   stoneBodyPosition=lstone.body.position
   
   var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y)
-  //console.log(distance)
- // console.log(lmango.r+lstone.r)
   	if(distance<=lmango.r+lstone.r)
     {
-      //console.log(distance);
-  	  Matter.Body.setStatic(lmango.body,false);
+      Matter.Body.setStatic(lmango.body,false);
     }
 
   }
